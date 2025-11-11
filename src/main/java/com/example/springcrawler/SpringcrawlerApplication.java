c503d48c9684568c35694f94f3179d49dc5ac802
Quân Đoàn <quandoan@Quans-MacBook-Air.local>
2025-11-11 14:25:01 +0700

    add first commit

---
+++ src/main/java/com/example/springcrawler/SpringcrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcrawlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springcrawler/SpringcrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
