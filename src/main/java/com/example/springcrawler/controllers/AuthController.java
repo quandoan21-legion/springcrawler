e9c46201ce544de196be99b119c54da899ddaf84
Juan-Legion-Doan <Juan@a1consulting.asia>
2025-11-15 15:51:55 +0700

    add auth service

--- src/main/java/com/example/springcrawler/controllers/AuthController.java
+++ src/main/java/com/example/springcrawler/controllers/AuthController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 @RequestMapping("api/v1/auth")
 public class AuthController {
